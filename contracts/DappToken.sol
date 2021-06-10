a133e5efd531197adf99fcdda3d1352426e039d1
Hasansattar <hasansattar650@gmail.com>
2021-06-10 18:30:38 +0500

    add name ,symbol,standard  and dynamic total supply

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,11 +1,31 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-contract DappToken{
-  uint256 public totalSupply;
-// constructor
-//set the total number of tokens
-//Read the total number of tokens
-  constructor () public {
-    totalSupply=1000000;
-  }
+contract DappToken {
+  //name
+    string public name = "Dapp Token";
+   //symbol
+    string public symbol = "Dapp";
+     //standard
+    string public standard = "Dapp Token v1.0";
+
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    // constructor
+    //set the total number of tokens
+    //Read the total number of tokens
+    constructor(uint256 _initialToken) public {
+        balanceOf[msg.sender] = _initialToken;
+        totalSupply = _initialToken;
+    }
+
+    //transfer
+    //exception if account doesnot have enough
+    //return a boolean
+    //transfer event
+
+    function transfer(address _to ,uint256 _value) public returns(bool success){
+           
+    }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -3,6 +3,28 @@ const DappToken= artifacts.require("./DappToken.sol");
 
 // eslint-disable-next-line no-undef
 contract('DappToken', function(accounts){
+     
+     var tokenInstance;
+it('initializes the contract with the correct values', function(){
+    return DappToken.deployed().then(function(instance){
+        tokenInstance= instance;
+        return tokenInstance.name();
+    }).then(function(name){
+        // eslint-disable-next-line no-undef
+        assert.equal(name,'Dapp Token', 'has the correct name');
+        return tokenInstance.symbol();
+    }).then(function(symbol){
+        // eslint-disable-next-line no-undef
+        assert.equal(symbol,'Dapp','has correct symbol');
+        return tokenInstance.standard();
+    }).then(function(standard){
+        // eslint-disable-next-line no-undef
+        assert.equal(standard,'Dapp Token v1.0','has the correct standard')
+    });
+});
+   
+
+
 
     it('sets the total supply upon deployment', function(){
           return DappToken.deployed().then(function(instance){
@@ -12,7 +34,14 @@ contract('DappToken', function(accounts){
           return tokenInstance.totalSupply();
           }).then(function(totalSupply){
           // eslint-disable-next-line no-undef
-          assert.equal(totalSupply.toNumber(),1000000,'sets the total supply to 1,000,000')
-          });
+          assert.equal(totalSupply.toNumber(),1000000,'sets the total supply to 1,000,000');
+ 
+             return tokenInstance.balanceOf(accounts[0]);
+
+         
+        }).then(function(adminBalance){
+            // eslint-disable-next-line no-undef
+            assert.equal(adminBalance.toNumber(),1000000,'it alloctaes the initial supply to admin account')
+        });
     });
 })
