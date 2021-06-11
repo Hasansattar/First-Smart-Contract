8d07c696a5e13063f8c5a8fea9923bbbb2009972
Hasansattar <hasansattar650@gmail.com>
2021-06-11 19:42:11 +0500

    transferForm function add

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -66,4 +66,20 @@ contract DappToken {
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
+    //transferFrom
+    function transferFrom(address _from,address _to,uint256 _value) public returns(bool success){
+         //Require _from has enough tokens
+         require(_value <= balanceOf[_from]);
+         //require allownace is big enough
+         require(_value <= allowance[_from][msg.sender]);
+         //change the balance
+         balanceOf[_from] -= _value;
+         balanceOf[_to] += _value;
+         // update the allowance
+         allowance[_from][msg.sender]-=_value;
+         //transfer event
+         emit Transfer(_from, _to, _value);
+         //return a boolean
+      return true;
+    }
 }
